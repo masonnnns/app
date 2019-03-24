5220b6f480356bd0a3b477c7d093548cac4384b5
Glitch (hello-express) <none>
2019-03-24 07:23:20 +0000

    🎑👲 Checkpoint ./server.lua:2551876/37

--- server.lua
+++ server.lua
@@ -6,18 +6,19 @@ local token = config.token
 local Utopia = require('utopia')
 local app = Utopia:new()
 
-client:on('ready', client()
+client:on('ready', function()
   client:setGame("Lua")
 	p('Logged in as '.. client.user.username)
 end)
 
-client:on('messageCreateclientfunction(message)
+client:on('messageCreate', function(message)
 	if message.content == pp..'ping' then
 		message.channel:send('Pong!')
 	end
 end)
 
-client:run('Bot '..tokenclientapp:use(function (req, res)
+client:run('Bot '..token)
+  app:use(function (req, res)
   res:finish('Check server.lua and remove the Utopia part (if you want)')
 end)
 
