0bec25255785aa5e57bc4d1fa3f85366572b5402
Glitch (invited-product) <none>
2020-01-31 21:50:34 +0000

    🔮🌏 Checkpoint ./commands/case.lua:647989/1072

--- commands/case.lua
+++ commands/case.lua
@@ -14,7 +14,21 @@ command.execute = function(message,args,client)
   local data = config.getConfig(message.guild.id)
   if args[2] == nil then
     return {success = false, msg = "You must provide a **case number** in argument 2."}
-  elseif data.modData.cases[]
+  elseif tonumber(args[2]) == nil then
+    return {success = false, msg = "Argument 2 must be a **number**."}
+  elseif data.modData.cases[tonumber(args[2])] == nil then
+    return {success = false, msg = "**Case "..args[2].."** doesn't exist."}
+  else
+    local case = data.modData.cases[tonumber(args[2])]
+    if string.lower(case.type) == "warn" then
+      message:reply{embed = {
+        title = "Warning - Case "..args[2],
+        description = "**Username:** "..client:getUser(case.user).tag.." (`"..case.user.."`)",
+      }}
+      return {success = "stfu", message = ""}
+    else
+      return {success = false, msg = "**Case "..args[2].."** couldn't be displayed."}
+    end
   end
 end
 
