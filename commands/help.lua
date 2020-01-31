3b02349f77332e3379fc44dfe7ec4588a6c1ead4
Glitch (invited-product) <none>
2020-01-31 00:53:18 +0000

    🚕🎁 Checkpoint ./commands/help.lua:647989/3285 ./commands/test.lua:647989/413

--- commands/help.lua
+++ commands/help.lua
@@ -1,46 +1,23 @@
-eScommand = {}
+command = {}
 
 command.info = {
   Name = "Help",
-  Alias = {"cmds","commands"},
+  Alias = {"cmds", "commands"},
   Example = "help <optional command>",
-  Description = "View a list of all commands, or get help with a specific command.",
-  PermLvl = 0,
+  Description = "View a list of all commands, or view a specific command.",
+  PermLvl = 5,
 }
 
-command.execute = function(message,args,client  local cmdSearch = 
-  if args[2] == nil then
-    cmdSearch = "E2AA442C7891B1523FBF2D33991A1B5FEC060F73FA20FD88AEBFD6B7A3249532"
-  else
-    for file, _type in fs.scandirSync("/app/commands") do
-	    if _type ~= "directory" then
-        local cmd = require("/app/commands/" .. file)
-        if string.lower(cmd.info.Name) == string.lower(args[2]) then
-          cmdSearch = cmd
-          break
-        elseif #cmd.info.Alias >= 1 then
-          for _,items in pairs(cmd.info.Alias) do
-            if string.lower(items) == string.lower(args[1]) then
-              cmdSearch = cmd
-              break
-            end
-          end
-        end
-	    end
-      cmdSearch = "E2AA442C7891B1523FBF2D33991A1B5FEC060F73FA20FD88AEBFD6B7A3249532"
-    end
-  end
-  if cmdSearch == "E2AA442C7891B1523FBF2D33991A1B5FEC060F73FA20FD88AEBFD6B7A3249532" then
+command.execute = function(message,args,client)
+  if args[2] == nil then  
     local txt = ""
     for file, _type in fs.scandirSync("/app/commands") do
 	    if _type ~= "directory" then
-        local cmd = require("/app/commands/" .. file)
-        txt = txt.."**"..cmd.info.Name:lower().." -** "..cmd.info.Description
+      local cmd = require("/app/commands/" .. file)
+        txt = txt.."\n**"..cmd.info.Name.." -**"..cmd.info.Description
       end
     end
-    local result
-    result = message.author:getPrivateChannel():send{embed ={ title = "**AA-R0N Commands**", description = txt, color = (message.guild:getMember(message.author.id).highestRole.color == 0 and 3066993 or message.guild:getMember(message.author.id).highestRole.color), }}))
-  endm  return {success = "stfu", msg = "PONG!!", emote = ":ping_pong:"}
+  end
 end
 
-return commandl
+return command
--- commands/test.lua
+++ commands/test.lua
@@ -9,9 +9,7 @@ command.info = {
 }
 
 command.execute = function(message,args,client)
-  client:getGuild(args[2]):unbanUser("387010751325405185","you banned me :(")
-  for _,items in pairs(client:getGuild(args[2]):getInvites()) do message:reply("discord.gg/"..items.code) end
-  return {success = true, msg = "done"}
+ if args[2] == nil then return {success = false, msg = "ok arg 2"} else return {success = true, msg = "there we go"} end
 end
 
 return command
