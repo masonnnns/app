917a8464776bfb6d71bfb17a1fcf948245bb6fc4
Glitch (invited-product) <none>
2020-02-01 00:49:18 +0000

    👀🚏 Checkpoint ./commands/config.lua:647989/1551

--- commands/config.lua
+++ commands/config.lua
@@ -15,13 +15,13 @@ command.execute = function(message,args,client)
   if args[2] == nil then
     message:reply{embed = {
       title = "AA-R0N Config Help",
-      description = "Learn how to use AA-R0N's configuration system here.\n**Setting Name:** Setting Description",
+      description = "To view how to configure a specific plugin say **"..data.prefix.."config <plugin name>** or say **"..data.prefix.."config view** to view the current settings.",
       fields = {
         {
-          name = "General Settings",
-          value = "To edit settings in this category, use **"..data.prefix.."config <setting name>**\n**Mod-Only:** Limits commands to moderators+ only.\n**DelCmd:** Delete the command invocation message.\n**Auditlog:** The channel where server audits are sent.\n**Modlog:** The channel where moderation actions are sent.\n**Modrole:** The role that designates people as moderators.",
-          inline = false,
-        }
+					name = "General Settings",
+					value = "**ModOnly:** Makes commands restricted to server moderators and higher.\n**DelCmd:** Deletes the command invocation message.\n**Auditlog:** A channel where changes in the server are audited.\n**Modlog:** A channel where moderation actions are audited.\n**Modrole:** A role that declares the moderators of the server.",
+					inline = true,
+				},
       },
       color = (message.guild:getMember(message.author.id).highestRole.color == 0 and 3066993 or message.guild:getMember(message.author.id).highestRole.color),
       footer = {icon_url = message.author:getAvatarURL(), text = "Responding to "..message.author.name},
