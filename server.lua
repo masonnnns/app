7a4ee41747db8f32f329335e40a0d2aee8a64df8
Glitch (invited-product) <none>
2020-02-02 00:46:07 +0000

    🌒🚋 Checkpoint ./server.lua:647989/3824

--- server.lua
+++ server.lua
@@ -70,12 +70,13 @@ client:on("ready", function()
       cache[guilds.id].channels[channels.id] = {name = channels.name, nsfw = channels.nsfw, ratelimit = channels.rateLimit, topic = (channels.topic ~= nil and channels.topic or "5FFA914BBF6B3D6149B228E8ED0AA2F1789C62227D4CEF4D9FE61D5E0F10597D"), permissions = channels.permissionOverwrites, position = channels.position, category = (channels.category == nil and "nil" or channels.category.id)}
       print("[CHANNEL CACHED]: "..channels.name.." has been cached in "..guilds.name..".")
     end
-    for _,channels in pairs(guild.categories) do
-      cache[guilds.id].channels[channels.id] = {name = channels.name, permissions = channels.permissionOverwrites, position = channel.position}
+    for _,channels in pairs(guilds.categories) do
+      cache[guilds.id].channels[channels.id] = {name = channels.name, permissions = channels.permissionOverwrites, position = channels.position}
       print('[CATEGORY CACHED]: '..channels.name.." has been cached in "..guilds.name..".")
     end
     for _,channels in pairs(guilds.voiceChannels) do
-      cache[guilds.id].channels[channels.id] = {name = channels.name, userlimit = channels.userLimit, bitrate = channels.bitrate}
+      cache[guilds.id].channels[channels.id] = {name = channels.name, userlimit = channels.userLimit, bitrate = channels.bitrate, permissions = channels.permissionOverwrites, category = (channels.category == nil and "nil" or channels.category.id)}
+      print("[VOICE CHANNEL CACHED]: "..channels.name.." has been cached in "..guilds.name..".")
     end
   end
 end)
@@ -276,7 +277,8 @@ client:on("channelCreate", function(channel)
   config[channel.guild.id] = configuration.getConfig(channel.guild.id)
   local channels = channel
   if channel.type == 0 then cache[channel.guild.id].channels[channels.id] = {name = channels.name, nsfw = channels.nsfw, ratelimit = channels.rateLimit, topic = (channels.topic ~= nil and channels.topic or "5FFA914BBF6B3D6149B228E8ED0AA2F1789C62227D4CEF4D9FE61D5E0F10597D"), permissions = channels.permissionOverwrites, position = channels.position, category = (channels.category == nil and "nil" or channels.category.id)} end
-  if channel.type == 4 then cache[channel.guilds.id].channels[channels.id] = {name = channels.name, permissions = channels.permissionOverwrites, position = channel.position} end
+  if channel.type == 4 then cache[channel.guild.id].channels[channels.id] = {name = channels.name, permissions = channels.permissionOverwrites, position = channel.position} end
+  if channel.type == 2 then cache[channel.guild.id].channels[channels.id] = {name = channels.name, userlimit = channels.userLimit, bitrate = channels.bitrate, permissions = channels.permissionOverwrites, category = (channels.category == nil and "nil" or channels.category.id)} end
   if config[channel.guild.id].auditlog == "nil" and channel.guild:getChannel(config[channel.guild.id].auditlog) == nil then return end
   local auditLog
   for a,items in pairs(channel.guild:getAuditLogs()) do if math.floor(items.createdAt) == os.time() or math.floor(items.createdAt) == os.time() - 1 or math.floor(items.createdAt) == os.time() + 1 or math.floor(items.createdAt) == os.time() + 2 and items.guild.id == member.guild.id and items.actionType == 10 then auditLog = items break end end
@@ -297,6 +299,26 @@ client:on("channelCreate", function(channel)
       channel.guild:getChannel(config[channel.guild.id].auditlog):send{embed ={ title = "Voice Channel Created", fields = { { name = "Channel", value = channel.name, inline = true, }, { name = "Channel Location", value = (channel.category == nil and "Not Categorized" or channel.category.name), inline = true, }, }, color = 11027200, }}
     else
       channel.guild:getChannel(config[channel.guild.id].auditlog):send{embed ={ title = "Voice Channel Created", fields = { { name = "Channel", value = channel.name, inline = true, }, { name = "Channel Location", value = (channel.category == nil and "Not Categorized" or channel.category.name), inline = true, }, { name = "Responsible Member", value = auditLog:getMember().mentionString.." (`"..auditLog:getMember().id.."`)", inline = false, }, }, color = 11027200, }}
+    end
+  end
+end)
+
+client:on("channelDelete", function(channel)
+  config[channel.guild.id] = configuration.getConfig(channel.guild.id)
+  if config[channel.guild.id].auditlog == "nil" and channel.guild:getChannel(config[channel.guild.id].auditlog) == nil then return end
+  local auditLog
+  for a,items in pairs(channel.guild:getAuditLogs()) do if math.floor(items.createdAt) == os.time() or math.floor(items.createdAt) == os.time() - 1 or math.floor(items.createdAt) == os.time() + 1 or math.floor(items.createdAt) == os.time() + 2 and items.guild.id == member.guild.id and items.actionType == 12 then auditLog = items break end end
+  if channels.type == 0 or channels.type == 2 then
+    if auditLog == nil then
+      channel.guild:getChannel(config[channel.guild.id].auditlog):send{embed ={ title = (channel.type == 2 and "Voice " or "").."Channel Deleted", fields = { { name = "Channel", value = channel.name.." (`"..channel.id.."`)", inline = true, }, { name = "Previous Location", value = (channel.category == nil and "Wasn't Categorized" or channel.category.name), inline = true, }, }, color = 10038562, }}
+    else
+      channel.guild:getChannel(config[channel.guild.id].auditlog):send{embed ={ title = (channel.type == 2 and "Voice " or "").."Channel Deleted", fields = { { name = "Channel", value = channel.name.." (`"..channel.id.."`)", inline = true, }, { name = "Previous Location", value = (channel.category == nil and "Wasn't Categorized" or channel.category.name), inline = true, }, { name = "Responsible Member", value = auditLog:getMember().mentionString.." (`"..auditLog:getMember().id.."`)", inline = false, }, }, color = 10038562, }}
+    end
+  elseif channels.type == 4 then
+    if auditLog == nil then
+      channel.guild:getChannel(config[channel.guild.id].auditlog):send{embed ={ title = "Category Deleted", fields = { { name = "Category", value = channel.name, inline = true, }, { name = "Previous Position", value = "#"..channel.position, inline = true, }, }, color = 12745742, }}
+    else
+      channel.guild:getChannel(config[channel.guild.id].auditlog):send{embed ={ title = "Category Deleted", fields = { { name = "Category", value = channel.name, inline = true, }, { name = "Previous Position", value = "#"..channel.position, inline = true, }, { name = "Responsible Member", value = auditLog:getMember().mentionString.." (`"..auditLog:getMember().id.."`)", inline = false, }, }, color = 12745742, }}
     end
   end
 end)
