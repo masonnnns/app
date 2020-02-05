04c83977e83e39be3a2890a37042e48d910e40a1
Glitch (pentagonal-healer) <none>
2020-02-05 23:55:46 +0000

    ⭐️🍵 Checkpoint ./commands/unmute.lua:647989/181

--- commands/unmute.lua
+++ commands/unmute.lua
@@ -16,13 +16,13 @@ command.execute = function(message,args,client)
   local data = config.getConfig(message.guild.id)
   if args[2] == nil then
     return {success = false, msg = "You must provide **a member to unmute** in argument 2."}
-  elseif #data.modData.actions = 0 then
+  elseif #data.modData.actions == 0 then
     return {success = false, msg = "There are currently **no muted users**."}
   else
-    local user = resolveUser.resolveUser(message,args[2])
+    local user = utils.resolveUser(message,args[2])
     if user == false then
       return {success = false, msg = "I couldn't find the user you mentioned."}
-    elseif resolveUser.getPermission(message,client,user.id) >= resolveUser.getPermission(message,client) then
+    elseif utils.getPermission(message,client,user.id) >= utils.getPermission(message,client) then
       return {success = false, msg = "You cannot "..command.info.Name:lower().." people with **higher than or equal permissions as you.**"}
     elseif user.id == client.user.id then
       return {success = false, msg = "I cannot "..command.info.Name:lower().." myself."}
@@ -34,6 +34,7 @@ command.execute = function(message,args,client)
       else
         local reason = (args[3] == nil and "No Reason Provided." or table.concat(args," ",3))
         table.remove(data.modData.actions,a)
+        user:removeRole(message.guild:getRole(data.mutedrole))
         data.modData.cases[1+#data.modData.cases] = {type = "Unmute", user = user.id, moderator = message.author.id, reason = reason}
         config.updateConfig(message.guild.id,data)
         message.guild:getChannel(data.modlog):send{embed = {
@@ -57,7 +58,7 @@ command.execute = function(message,args,client)
           },
           color = 2067276,
         }}
-        return {success = false, msg = "**"..user.username.."** has been unmuted."}
+        return {success = true, msg = "**"..user.username.."** has been unmuted."}
       end
     end
   end
