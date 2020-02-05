a31dfc685ab5c16020d7799d04d7d4ec8148a229
Glitch (invited-product) <none>
2020-02-05 23:07:46 +0000

    🐏🕋 Checkpoint ./commands/moderations.lua:647989/1767

--- commands/moderations.lua
+++ commands/moderations.lua
@@ -1,7 +1,7 @@
 command = {}
 
 local config = require("/app/config.lua")
-local utils = require("/app/resolve-user.lua")
+local utils = require("/app/utils.lua")
 local cache = require("/app/server.lua")
 
 command.info = {
@@ -14,6 +14,19 @@ command.info = {
 
 command.execute = function(message,args,client)
   local data = config.getConfig(message.guild.id)
+  if #data.modData.actions == 0 then
+    return {success = false, msg = "There are no **active moderations** to display."}
+  else
+    local txt = ""
+    for _,items in pairs(data.modData.actions) do txt = txt.."\n**"..client:getUser(items.user).tag.." `["..items.type:upper().."]` - **"..(tostring(items.duration):lower() == "permanent" and "Permanent" or utils.getTimeString(items.duration - os.time())) end
+    message:reply{embed = {
+      title = "Moderations ["..#data.modData.actions.."]",
+      description = txt,
+      footer = {icon_url = message.author:getAvatarURL(), text = "Responding to "..message.author.name},
+      color = (cache.getCache("roleh",message.guild.id,message.author.id).color == 0 and 3066993 or cache.getCache("roleh",message.guild.id,message.author.id).color),
+    }}
+    return {success = "stfu"}
+  end
 end
 
 return command
