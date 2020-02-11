a92476113b693c61ccb2284734649d8c1e8495ea
Glitch (pentagonal-healer) <none>
2020-02-11 23:29:11 +0000

    😋⛄️ Checkpoint ./commands/add.lua:647989/1024 ./config.lua:647989/28

--- commands/add.lua
+++ commands/add.lua
@@ -18,7 +18,16 @@ command.execute = function(message,args,client)
   local data = config.getConfig(message.guild.id)
   if data.tickets.enabled == false then return {success = "stfu"} end
   if data.tickets.category == "nil" or message.guild:getChannel(data.tickets.category) == nil then
-    return {success = false, msg = "**Config Error:** There is no tickets category setup."}
+    return {success = false, msg = "**Config Error:** There is no "..(data.tickets.category == "nil" and "" or "valid ").."tickets category setup."}
+  else
+    local found = 0 for _,items in pairs(data.tickets.channels) do if items.user == message.author.id then found = found + 1 end end
+    if found + 1 > data.tickets.max then
+      return {success = false, msg = "You already have **"..found.." ticket"..(found == 1 and "" or "s").."** open, you cannot open anymore."}
+    else
+      local channel = message.guild:createTextChannel("ticket-"..data.tickets.ticket+1)
+      if channel == nil or channel == false then return {success = false, msg = "**Config Error:** I couldn't create the ticket, make sure I have permissions!"} end
+      channel:setCategory(data.tickets.category)
+    end
   end
 end
 
