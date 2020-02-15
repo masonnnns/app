aa49e4840e294a7edceb7ffc385290c73e76aae2
Glitch (pentagonal-healer) <none>
2020-02-15 12:12:57 +0000

    🛰💎 Checkpoint ./commands/unban.lua:647989/4155

---
+++ commands/unban.lua
@@ -0,0 +1,62 @@
+command = {}
+
+local config = require("/app/config.lua")
+local utils = require("/app/resolve-user.lua")
+local cache = require("/app/server.lua")
+
+command.info = {
+  Name = "Unban",
+  Alias = {},
+  Usage = "unban <user> <reason>",
+  Category = "Moderation",
+  Description = "Unbans the specified user.",
+  PermLvl = 1,
+}
+
+command.execute = function(message,args,client)
+  local data = config.getConfig(message.guild.id)
+  if args[2] == nil then
+    return {success = false, msg = "You must provide **a member to unban** in argument 2."}
+  else
+    local user = client:getUser(args[2])
+    if user == nil then
+      return {success = false, msg = "I couldn't find the user you mentioned."}
+    elseif message.guild:getMember(client.user.id):hasPermission("banMembers") == false then
+      return {success = false, msg = "I need the **Ban Members** permission to this."}
+    elseif message.guild:getBan(user.id) == nil then
+      return {success = false, msg = "**"..user.name.."** isn't banned!"}
+    else
+      local reason = (args[3] == nil and "No Reason Provided." or table.concat(args," ",3))
+      for a,items in pairs(data.modData.actions) do if items.id == user.id then table.remove(data.modData.actions,a) end end
+      message.guild:unbanUser(user.id,reason)
+      data.modData.cases[1+#data.modData.cases] = {type = "Unban", user = user.id, moderator = message.author.id, reason = reason, id = 0}
+      if data.modlog ~= "nil" and message.guild:getChannel(data.modlog) ~= nil then
+        local msg = message.guild:getChannel(data.modlog):send{embed = {
+          title = "Unban - Case "..#data.modData.cases,
+          fields = {
+            {
+              name = "Member",
+              value = user.tag.." (`"..user.id.."`)",
+              inline = false,
+            },
+            {
+              name = "Reason",
+              value = reason,
+              inline = false,
+            },
+            {
+              name = "Responsible Moderator",
+              value = message.author.mentionString.." (`"..message.author.id.."`)",
+              inline = false,
+            },
+          },
+           color = 2067276,
+        }}
+        data.modData.cases[#data.modData.cases].id = msg.id
+        return {success = true, msg = "**"..user.username.."** has been unbanned. `[Case #"..#data.modData.cases.."]`"}
+      end
+    end
+  end
+end
+
+return command
