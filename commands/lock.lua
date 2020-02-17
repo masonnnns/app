c79b471404fafc04f7e2a37af2e9c46bb8902baf
Glitch (pentagonal-healer) <none>
2020-02-17 11:10:11 +0000

    👛🌶 Checkpoint ./commands/lock.lua:647989/2967 ./commands/ban.lua:647989/4

--- commands/lock.lua
+++ commands/lock.lua
@@ -1,12 +1,12 @@
 command = {}
 
 local config = require("/app/config.lua")
-local utils = require("/resolve-user.lua")
+local utils = require("/app/resolve-user.lua")
 
 command.info = {
   Name = "Lock",
   Alias = {},
-  Usage = "lock <optional channel/category> <optional duration> <optional reason>",
+  Usage = "lock <optional channel/category> <optional reason>",
   Category = "Administration",
   Description = "Lock a channel or set of channels.",
   PermLvl = 2,
@@ -14,13 +14,23 @@ command.info = {
 
 command.execute = function(message,args,client)
   local data = config.getConfig(message.guild.id)
-  local lockData = {channel = "", reason = "", duration = ""}
+  local lockData = {channel = "", reason = ""}
   if args[2] == nil then
     lockData.channel = message.channel.id
     lockData.reason = "No Reason Provided"
-    lockData.duration = "Permanent"
-    message:reply()
+  elseif utils.resolveChannel(message,args[2]) ~= false then
+    lockData.channel = utils.resolveChannel(message,args[2]).id
+    locakData.reason = (args[3] == nil and "No Reason Provided" or table.concat(args, " ", 3))
+  elseif utils.resolveCategory(message,args[2]) ~= false then
+    lockData.channel = utils.resolveChannel(message,args[2]).id
+    locakData.reason = (args[3] == nil and "No Reason Provided" or table.concat(args, " ", 3))
+  else
+    lockData.channel = message.channel.id
+    lockData.reason = (args[2] == nil and "No Reason Provided" or table.concat(args, " ", 3))
   end
+  if message.guild:getChannel(lockData.channel)
 end
+
+-- <:aaronlock:678918427523678208>
 
 return command
