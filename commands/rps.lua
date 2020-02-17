fd1821f661f3666f60cba8a7c893f0be59fbae19
Glitch (pentagonal-healer) <none>
2020-02-17 10:46:08 +0000

    ✂️🏤 Checkpoint ./commands/rps.lua:647989/849 ./config.lua:647989/85

--- commands/rps.lua
+++ commands/rps.lua
@@ -25,20 +25,21 @@ end
 
 command.execute = function(message,args,client)
   local data = config.getConfig(message.guild.id)
-  if data.games.enabled == false then return {success = false, msg = "The **games** plugin is **disabled**."} end
   if args[2] == nil then
     return {success = false, msg = "You must provide **rock, paper or scissors** in argument 2."}
   elseif args[2]:lower() ~= "rock" and args[2]:lower() ~= "paper" and args[2]:lower() ~= "scissors" and args[2]:lower() ~= "r" and args[2]:lower() ~= "p" and args[2]:lower() ~= "s" then
     return {success = false, msg = "You must provide **rock, paper or scissors** in argument 2."}
   else
     local botNum, theirNum = 0, 0
-    local tickets = 0
     args[2] = args[2]:lower()
     if args[2] == "rock" or args[2] == "r" then
+      botNum = math.random(1,2)
       theirNum = 1
     elseif args[2] == "paper" or args[2] == "p" then
+      botNum = math.random(1,3)
       theirNum = 2
     elseif args[2] == "scissors" or args[2] == "s" then
+      botNum = math.random(2,3)
       theirNum = 3
     else
       return {success = false, msg = "Invalid arg 2."}
@@ -47,28 +48,22 @@ command.execute = function(message,args,client)
       message:reply("We both selected **"..getObj(theirNum):lower().."**, it's a tie.")
     elseif botNum == 1 then
       if theirNum == 2 then
-        message:reply("**Paper** beats **rock**, you win.\n:tickets: **+5 Tickets**")
-        tickets = 5
+        message:reply("**Paper** beats **rock**, you win.")
       elseif theirNum == 3 then
         message:reply("**Rock** beats **scissors**, I win.")
       end
     elseif botNum == 2 then
       if theirNum == 3 then
-        message:reply("**Scissors** beats **paper**, you win.\n:tickets: **+5 Tickets**")
-        tickets = 5
+        message:reply("**Scissors** beats **paper**, you win.")
       elseif theirNum == 1 then
         message:reply("**Paper** beats **rock**, I win.")
       end
     elseif botNum == 3 then
       if theirNum == 1 then
-        message:reply("**Rock** beats **scissors**, you win.\n:tickets: **+5 Tickets**")
-        tickets = 5
+        message:reply("**Rock** beats **scissors**, you win.")
       elseif theirNum == 2 then
         message:reply("**Paper** beats **rock**, I win.")
       end
-    end
-    if tickets > 0 then
-      -- add tickets
     end
     return {success = "stfu"}
   end
