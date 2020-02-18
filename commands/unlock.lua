4c5d6215e8744ebd93c79ff2be5a1905d72f2de2
Glitch (pentagonal-healer) <none>
2020-02-18 20:53:58 +0000

    🐌🏬 Checkpoint ./commands/lock.lua:647989/899 ./commands/unlock.lua:647989/3795

--- commands/unlock.lua
+++ commands/unlock.lua
@@ -4,11 +4,11 @@ local config = require("/app/config.lua")
 local utils = require("/app/resolve-user.lua")
 
 command.info = {
-  Name = "Lock",
+  Name = "Unlock",
   Alias = {},
-  Usage = "lock <optional channel> <optional reason>",
+  Usage = "unlock <optional channel>",
   Category = "Moderation",
-  Description = "Lock a channel or set of channels.",
+  Description = "Unlock a locked channel.",
   PermLvl = 1,
 }
 
@@ -18,26 +18,27 @@ command.execute = function(message,args,client)
   if message.guild:getMember(client.user.id):hasPermission("manageChannels") == false then return {success = false, msg = "I need the **Manage Channels** permission to do this."} end
   if args[2] == nil then
     lockData.channel = message.channel.id
-    lockData.reason = "No Reason Provided"
   elseif utils.resolveChannel(message,args[2]) ~= false then
     lockData.channel = utils.resolveChannel(message,args[2]).id
-    lockData.reason = (args[3] == nil and "No Reason Provided" or table.concat(args, " ", 3))
   else
     lockData.channel = message.channel.id
-    lockData.reason = (args[2] == nil and "No Reason Provided" or table.concat(args, " ", 3))
   end
   local channel = message.guild:getChannel(lockData.channel)
   if channel.type == 0 then
-    if channel:getPermissionOverwriteFor(message.guild:getRole(message.guild.id)):getDeniedPermissions():has("sendMessages") or channel:getPermissionOverwriteFor(message.guild:getRole(message.guild.id)):getDeniedPermissions():has("readMessages") then return {success = false, msg = "**"..channel.mentionString.."** is already locked."} end
-    local success = channel:getPermissionOverwriteFor(message.guild:getRole(message.guild.id)):denyPermissions("sendMessages")
+    if channel:getPermissionOverwriteFor(message.guild:getRole(message.guild.id)):getDeniedPermissions():has("readMessages") then
+      return {success = false, msg = channel.mentionString.." is a private channel."}
+    elseif channel:getPermissionOverwriteFor(message.guild:getRole(message.guild.id)):getAllowedPermissions():has("sendMessages") then
+      return {success = false, msg = channel.mentionString.." isn't locked."}
+    end
+    local success = channel:getPermissionOverwriteFor(message.guild:getRole(message.guild.id)):allowPermissions("sendMessages")
     if not success then return {success = false, msg = "I need the **Manage Channels** permission to do this."} end
     if channel.id ~= message.channel.id then
       channel:send("<:aaronlock:678918427523678208> This channel has been locked by server staff.\n**Reason:** "..lockData.reason)
     end
     data.modData.locked[channel.id] = message.author.id
-    return {success = true, msg = "**"..channel.mentionString.."** has been locked."}
+    return {success = true, msg = "**"..channel.mentionString.."** has been unlocked."}
   else
-    return {success = false, msg = "I can only lock **text channels**."}
+    return {success = false, msg = "I can only unlock **text channels**."}
   end
 end
 
