4bd01425748c7602cc0be307a0f702bc1a7e0f71
Glitch (pentagonal-healer) <none>
2020-02-20 02:28:57 +0000

    🔋🤗 Checkpoint ./commands/ping.lua:647989/55

--- commands/ping.lua
+++ commands/ping.lua
@@ -11,6 +11,7 @@ command.info = {
 
 command.execute = function(message,args,client)
   local m = message:reply(":ping_pong: Ping?")
+  if m == nil then return {success = "stfu"} end
   local latency = m.createdAt - message.createdAt
   m:setContent(":ping_pong: Pong! `"..math.max(latency).."ms`")
   return {success = "stfu", msg = "PONG!!", emote = ":ping_pong:"}
