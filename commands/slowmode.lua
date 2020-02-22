1abf755dbfd0811e7929a86121ff38beee706e55
Glitch (pentagonal-healer) <none>
2020-02-22 14:22:28 +0000

    👽🏯 Checkpoint ./commands/slowmode.lua:647989/171 ./.gitignore:647989/26 ./deps/discordia/libs/containers/GuildTextChannel.lua:647989/8248

--- commands/slowmode.lua
+++ commands/slowmode.lua
@@ -18,19 +18,20 @@ command.execute = function(message,args,client)
   elseif args[2] == nil then
     return {success = false, msg = "You must provide a **duration** in argument 2."}
   else
+    local channel = message.guild:getChannel(message.channel.id)
     if args[2]:lower() == "clear" then
-      message.channel:setRateLimit(0)
+      channel:setRateLimit(0)
       return {success = true, msg = "**Cleared** this channel's slowmode."}
     elseif tonumber(args[2]) == nil then
       return {success = false, msg = "Argument 2 must be a **number** or **\"clear\"**."}
-    elseif tonumber(args[2]) > 120 or tonumber(args[2]) < 0 then
-      return {success = false, msg = "Argument 2 must be between **0 and 120**."}
+    elseif tonumber(args[2]) > 21600 or tonumber(args[2]) < 0 then
+      return {success = false, msg = "Argument 2 must be between **0 and 21600**."}
     else
-      message.channel:setRateLimit(tonumber(args[2]))
+      channel:setRateLimit(tonumber(args[2]))
       if tonumber(args[2]) == 0 then
         return {success = true, msg = "**Cleared** this channel's slowmode."}
       else
-        return {success = true, msg = "Set this channel's slowmode to **"..args[2].." second"..(tonumber(args[2]) == 1 and "" or "s").." per message**."}
+        return {success = true, msg = "Set this channel's slowmode to **"..channel.rateLimit.." second"..(channel.rateLimit == 1 and "" or "s").." per message**."}
       end
     end
   end
