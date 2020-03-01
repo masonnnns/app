f1c499aadbea83e7433382381468b43e89e30b59
Glitch (pentagonal-healer) <none>
2020-03-01 18:02:17 +0000

    ⏳🍃 Checkpoint ./server.lua:647989/2361 ./commands/ping.lua:647989/526

--- server.lua
+++ server.lua
@@ -9,9 +9,27 @@ local client = discordia.Client {
 local config = require("/app/config.lua")
 config.setupConfigs('xddd')
 
+local function sepMsg(msg)
+	local Args = {}
+	local Command = msg
+	for Match in Command:gmatch("[^%s]+") do
+	table.insert(Args, Match)
+	end;
+	local Data = {
+	["MessageData"] = Message;
+	["Args"] = Args;
+	}
+	return Args
+end
+
 client:on("messageCreate",function(message)
+  if message.author.id ~= client.owner.id then return end 
   if message.author.bot or message.guild.id == nil then return false end
   local data = config.getConfig(message.guild.id)
+  if string.sub(message.content,1,string.len(data.general.prefix)) == data.general.prefix then
+    local args = sepMsg(string.sub(message.content,string.len(data.general.prefix)+1))
+    message:reply(table.concat(args,", "))
+  end
 end)
 
 client:run("Bot NDYzODQ1ODQxMDM2MTE1OTc4.Xlvwig.pblOapFexh1F51CIbnqEi3XHWEA")
