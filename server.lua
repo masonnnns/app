2461d4dda8db0ac0401354b5fb9c69ea562933ef
Glitch (pentagonal-healer) <none>
2020-03-01 18:28:11 +0000

    ⛳️⏱ Checkpoint ./utils.lua:647989/993 ./server.lua:647989/1332

--- server.lua
+++ server.lua
@@ -29,22 +29,37 @@ client:on("messageCreate",function(message)
   if string.sub(message.content,1,string.len(data.general.prefix)) == data.general.prefix then
     local args = sepMsg(string.sub(message.content,string.len(data.general.prefix)+1))
     local found
-    for cmds, _type in require("fs").scandirSync("./commands") do
+    for file, _type in require("fs").scandirSync("./commands") do
       if _type ~= "directory" then
         local command = require("./commands/"..file)
         if string.lower(args[1]) == string.lower(file) then
-          found = cmd break
+          found = command break
         elseif #command.info.Alias >= 1 then
           for _,items in pairs(command.info.Alias) do
             if string.lower(items) == string.lower(args[1]) then
-              found = cmd break
+              found = command break
             end
           end
         end
       end
     end
-    if found ~= nil then
-      
+    if found == nil or require("/app/utils.lua").Permlvl(message,client) == 0 and data.modonly == true or require("/app/utils.lua").Permlvl(message,client) < command.info.PermLvl then
+      if found ~= nil and data.modonly == false then 
+          local m = message:reply("<:aforbidden:678187354242023434> You **don't have permissions** to use this command!")
+          require("timer").sleep(5000)
+          m:delete()
+      end
+    else
+      local execute = found.execute(message,args,client)
+      if execute == nil or type(execute) ~= "table" then
+        message:reply("<:atickno:678186665616998400> An **unknown error** occured.")
+      elseif execute.success == false then
+        message:reply("<:atickno:678186665616998400> "..execute.msg)
+      elseif tostring(execute.success):lower() == "stfu" then
+        -- stfu literally
+      else
+        message:reply((execute.emote == nil and "<:atickyes:678186418937397249>" or execute.emote).."  "..execute.msg)
+      end
     end
   end
 end)
--- utils.lua
+++ utils.lua
@@ -1,8 +1,24 @@
 module = {}
 
-module.Permlvl = function(msg,client)
+module.Permlvl = function(msg,client,id)
+  if id == nil then id = msg.author.id end
   local data = require("/app/config.lua").getConfig(message.guild.id)
-  if message.guild
+  local found
+  for items,_ in pairs(msg.guild.members) do if items == id then found = true break end
+  if found == nil then return 0 end
+  if id == message.guild.ownerId then
+		return 3
+	elseif message.guild:getMember(id):hasPermission("administrator") == true then
+		return 2
+	elseif message.guild:getMember(id):hasPermission("manageGuild") == true then
+		return 2
+  elseif #data.general.modroles >= 1 then
+    for _,items in pairs(data.general.modroles) do
+      if message.guild:getMember(id):hasRole(items) then return 1 end
+    end
+  else
+    return 0
+  end
 end
 
 return module
