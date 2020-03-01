56ba227a67f42425e85aa11d5d16de40a8576055
Glitch (pentagonal-healer) <none>
2020-03-01 22:54:06 +0000

    🌥🍞 Checkpoint ./commands/members.lua:647989/19

--- commands/members.lua
+++ commands/members.lua
@@ -15,12 +15,12 @@ command.info = {
 command.execute = function(message,args,client)
   local data = config.getConfig(message.guild.id)
   if args[2] == nil then
-    message:reply(embed = {
-      title = "Members of "..guild.name,
+    message:reply{embed = {
+      title = "Members of "..message.guild.name,
       description = "We have **"..utils.addCommas(#message.guild.members).."** members!",
       footer = {icon_url = message.author:getAvatarURL(), text = "Responding to "..message.author.name},
       color = (message.guild:getMember(message.author.id).highestRole.color == 0 and 3066993 or message.guild:getMember(message.author.id).highestRole.color), 
-    },)
+    }}
     return {success = 'stfu'}
   else
     local role = utils.resolveRole(message,table.concat(args," ",2))
