a1f12eba9d932b741b2b35162c4111c2e14f44c0
Glitch (pentagonal-healer) <none>
2020-03-21 12:37:49 +0000

    ⏰🍅 Checkpoint ./commands/config.lua:647989/1511

--- commands/config.lua
+++ commands/config.lua
@@ -26,11 +26,17 @@ command.execute = function(message,args,client)
         {name = "Mod Only Commands", value = (data.general.modonly and "Enabled." or "Disabled."), inline = true},
         {name = "Audit Log", value = (data.general.auditlog == "nil" and "Disabled." or (message.guild:getChannel(data.general.auditlog) == nil and "Disabled." or message.guild:getChannel(data.general.auditlog).mentionString)), inline = true},
         {name = "Muted Role", value = (data.general.mutedrole == "nil" and "Disabled." or (message.guild:getRole(data.general.mutedrole) == nil and "Disabled." or message.guild:getRole(data.general.mutedrole).mentionString)), inline = true},
-        {name = "Moderation Log", value = (data.general.modlog == "nil" and "Disabled." or (message.guild:getChannel(data.general.modlog) == nil and "Disabled." or message.guild:getChannel(data.general.modlog).mentionString)), inline = true}
+        {name = "Moderation Log", value = (data.general.modlog == "nil" and "Disabled." or (message.guild:getChannel(data.general.modlog) == nil and "Disabled." or message.guild:getChannel(data.general.modlog).mentionString)), inline = true},
+        {name = "Moderator Roles [0]", value = "", inline = false},
+        {name = "Audit Ignored Channels [0]", value = "s", inline = false},
       },
       footer = {icon_url = message.author:getAvatarURL(), text = "Responding to "..message.author.tag},
       color = (message.guild:getMember(message.author.id).highestRole.color == 0 and 3066993 or message.guild:getMember(message.author.id).highestRole.color),
     }
+    local num = 0
+    for _,items in pairs(data.general.modroles) do local role = message.guild:getRole(items) if role then num = num + 1 if pages[1].fields[7].value == "" then pages[1].fields[7].value = role.mentionString else pages[1].fields[7].value = pages[1].fields[7].value..", "..role.mentionString end end end
+    pages[1].fields[7].name = "Moderator Roles ["..num.."]"
+    if num == 0 then pages[1].fields[7].value = "None set!" end
     require("/app/pages.lua").addDictionary(message,pages,message.author.id)
   end
   return {success = "stfu"}
