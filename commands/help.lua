c724f6b25ce4a191a319623a8d206c52ae6d178a
Glitch (stellar-cosmic-lasagna) <none>
2020-03-21 19:56:22 +0000

    🍓🍠 Checkpoint ./commands/help.lua:647989/2005

--- commands/help.lua
+++ commands/help.lua
@@ -1,6 +1,7 @@
 command = {}
 
 local utils = require("/app/utils.lua")
+local fs = require('fs')
 
 command.info = {
   Name = "Help",
@@ -18,15 +19,29 @@ command.execute = function(message,args,client)
   for file, _type in fs.scandirSync("/app/commands") do
 	  if _type ~= "directory" then
       local cmd = require("/app/commands/" .. file)
-      if cmd.Name:lower() == commandSearch:lower() then found = file break end
-      for _,items in pairs(cmd.Alias) do if items:lower() == commandSearch:lower() then found = file break end
+      if cmd.info.Name:lower() == commandSearch:lower() then found = file break end
+      for _,items in pairs(cmd.info.Alias) do if items:lower() == commandSearch:lower() then found = file break end
     end
   end
   if found == false then
     local embed = {
       title = "Commands List",
-      description = "To use a command, say **"..data.general.prefix.."
+      description = "To use a command, say **"..data.general.prefix.."<command name>**.\nTo view more info about a command say **"..data.general.prefix.."help <command name>**.\n**[Support Server](https://discordapp.com/invite/PjKaAXx) - [Bot Invite](https://discordapp.com/oauth2/authorize?client_id=414030463792054282&scope=bot&permissions=502787319)**",
+      fields = {},
+      color = (message.guild:getMember(message.author.id).highestRole.color == 0 and 3066993 or message.guild:getMember(message.author.id).highestRole.color),
     }
+    for file, _type in fs.scandirSync("/app/commands") do
+      if _type ~= "directory" then
+        local cmd = require("/app/commands/" .. file)
+        if cmd.info.PermLvl == 5 and message.author.id ~= client.owner.id then else
+          if cmd.info.Category == nil then cmd.info.Category = "Misc" end
+          if #embed.fields == 0 then embed.fields[1] = {name = cmd.info.Category, value = "`"..cmd.info.Name.."`", inline = false} end
+          for _,items in pairs(embed.fields) do if items.name == cmd.info.Category then items.value = items.value.." , `"..cmd.info.Name.."`" else embed.fields[1+#embed.fields] = {name = cmd.info.Category, value = "`"..cmd.info.Name.."`", inline = false} end end
+        end
+      end
+    end
+    message:reply{embed = embed}
+  end
   end
 end
 
