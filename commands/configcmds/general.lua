b4742a56f694d530edc81916145af9106b7e3d4e
Glitch (pentagonal-healer) <none>
2020-03-21 14:21:49 +0000

    🖖📍 Checkpoint ./commands/configcmds/general.lua:647989/2057

--- commands/configcmds/general.lua
+++ commands/configcmds/general.lua
@@ -13,7 +13,24 @@ command = function(message,args,client,data)
   elseif args[3] == "modonly" then
     data.general.modonly = not data.general.modonly
     return {success = true, msg = "**"..(data.general.modonly and "Enabled" or "Disabled").."** the **moderator only** setting."}
-
+  elseif args[3] == "auditlog" or args[3] == "audit" then
+    if args[4] == nil then return {success = false, msg = "You must provide an auditlog channel."} end
+    local channel = require("/app/utils.lua").resolveChannel(message,table.concat(args," ",4))
+    if channel == false then return {success = false, msg = "I couldn't find the channel you mentioned."} end
+    data.general.auditlog = channel.id
+    return {success = true, msg = "Set the **auditlog channel** to "..channel.mentionString.."."}
+  elseif args[3] == "modlog" then
+    if args[4] == nil then return {success = false, msg = "You must provide a modlog channel."} end
+    local channel = require("/app/utils.lua").resolveChannel(message,table.concat(args," ",4))
+    if channel == false then return {success = false, msg = "I couldn't find the channel you mentioned."} end
+    data.general.modlog = channel.id
+    return {success = true, msg = "Set the **modlog channel** to "..channel.mentionString.."."}
+  elseif args[3] == "mutedrole" or args[3] == "muted" then
+    if args[4] == nil then return {success = false, msg = "You must provide a muted role."} end
+    local channel = require("/app/utils.lua").resolveRole(message,table.concat(args," ",4))
+    if channel == false then return {success = false, msg = "I couldn't find the role you mentioned."} end
+    data.general.mutedrole = channel.id
+    return {success = true, msg = "Set the **muted role** to **"..channel.name.."**."}
   end
 end
 
