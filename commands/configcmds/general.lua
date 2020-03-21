63632cc39c1ed9afd7ff878e85f70c9bcbc19ab8
Glitch (pentagonal-healer) <none>
2020-03-21 13:57:49 +0000

    😬🎓 Checkpoint ./commands/configcmds/general.lua:647989/820 ./commands/case.lua:647989/8 ./commands/uptime.lua:647989/1214

--- commands/configcmds/general.lua
+++ commands/configcmds/general.lua
@@ -2,7 +2,13 @@ command = {}
 
 command = function(message,args,client,data)
   if args[3] ~= nil then args[3] = args[3]:lower() end
-  if args[3] == "prefix"
+  if args[3] == "prefix" then
+    if args[4] == nil then return {success = false, msg = "You must provide a **new prefix**."} end
+    if string.len(args[4]) >= 11 then return {success = false, msg = "The prefix must be **less than 10 characters**."} end
+    data.general.prefix = args[4]:lower()
+    return {success = true, msg = "Set the prefix to `"..data.general.prefix.."`."}
+  elseif args[3] ==
+  end
 end
 
 return command
---
+++ commands/uptime.lua
@@ -0,0 +1,27 @@
+command = {}
+
+local utils = require("/app/utils.lua")
+local cache = require("/app/server.lua")
+
+command.info = {
+  Name = "Uptime",
+  Alias = {"up"},
+  Usage = "uptime",
+  Category = "Information",
+  Description = "Shows how long the bot has been online.",
+  PermLvl = 0,
+}
+
+local start = os.time()
+
+command.execute = function(message,args,client)
+    message:reply{embed = {
+      title = "Uptime",
+      description = utils.getTimeString(os.time() - start)..".",
+      footer = {icon_url = message.author:getAvatarURL(), text = "Responding to "..message.author.name},
+      color = (message.guild:getMember(message.author.id).highestRole.color == 0 and 3066993 or message.guild:getMember(message.author.id).highestRole.color),
+    }}
+  return {success = "stfu", msg = "PONG!!", emote = ":ping_pong:"}
+end
+
+return command
