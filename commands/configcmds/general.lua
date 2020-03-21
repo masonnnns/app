c46ea2bb099ab812ce2d00d599ba6c767cda5980
Glitch (pentagonal-healer) <none>
2020-03-21 14:09:49 +0000

    🌴🏤 Checkpoint ./commands/configcmds/general.lua:647989/1060

--- commands/configcmds/general.lua
+++ commands/configcmds/general.lua
@@ -7,7 +7,13 @@ command = function(message,args,client,data)
     if string.len(args[4]) >= 11 then return {success = false, msg = "The prefix must be **less than 10 characters**."} end
     data.general.prefix = args[4]:lower()
     return {success = true, msg = "Set the prefix to `"..data.general.prefix.."`."}
-  elseif args[3] ==
+  elseif args[3] == "delcmd" then
+    data.general.delcmd = not data.general.delcmd
+    return {success = true, msg = "**"..(data.general.delcmd and "Enabled" or "Disabled").."** the **delete invocation message** setting."}
+  elseif args[3] == "modonly" then
+    data.general.modonly = not data.general.modonly
+    return {success = true, msg = "**"..(data.general.modonly and "Enabled" or "Disabled").."** the **moderator only** setting."}
+
   end
 end
 
