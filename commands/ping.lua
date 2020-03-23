f16b63672226cdf8c4101f9614c338581e323e62
Glitch (stellar-cosmic-lasagna) <none>
2020-03-23 11:45:01 +0000

    🚧🌄 Checkpoint ./commands/ping.lua:647989/1103

--- commands/ping.lua
+++ commands/ping.lua
@@ -1,5 +1,19 @@
 command = {}
 
+local function parse(num)
+num = math.max(num)
+num = tostring(num)
+print(num)
+local pos, tables, done = 1, {}, false
+repeat
+    if string.sub(num,pos) ~= "." then tables[pos] = string.sub(num,pos) pos = pos+1 else done = true end
+    require("timer").sleep(250)
+until pos >= string.len(num) or done == true
+  num = table.concat(tables,"")
+  print(num)
+  return num
+end
+
 command.info = {
   Name = "Ping",
   Alias = {},
@@ -10,13 +24,13 @@ command.info = {
 }
 
 command.execute = function(message,args,client)
-  local m = message:reply(":ping_pong: Ping?")
+  local m = message:reply(":ping_pong: Ping")
   if m == nil then return {success = "stfu"} end
-  local latency = m.createdAt - message.createdAt
-  m:setContent(":ping_pong: Ping?!")
-  local edited = discordia.Date.fromISO(m.editedTimestamp):toSeconds() - m.createdAt
+  local latency = require("discordia").Date.fromISO(m.timestamp):toMilliseconds() - require("discordia").Date.fromISO(message.timestamp):toMilliseconds()
+  m:setContent(":ping_pong: Ping? `"..parse(latency).."ms`")
+  local edited = require("discordia").Date.fromISO(m.editedTimestamp):toMilliseconds() - require("discordia").Date.fromISO(m.timestamp):toMilliseconds()
   latency = latency + edited
-  m:setContent(":ping_pong: Pong! `"..math.max(latency).."ms`")
+  m:setContent(":ping_pong: Pong! `"..parse(latency).."ms`")
   return {success = "stfu", msg = "PONG!!", emote = ":ping_pong:"}
 end
 
