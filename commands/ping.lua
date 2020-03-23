a934d2b97ca4e89060069be9b6547164310813c2
Glitch (stellar-cosmic-lasagna) <none>
2020-03-23 11:57:01 +0000

    😻🚞 Checkpoint ./commands/ping.lua:647989/185

--- commands/ping.lua
+++ commands/ping.lua
@@ -2,15 +2,13 @@ command = {}
 
 local function parse(num)
 num = math.max(num)
-num = tostring(num)
-print(num)
+local str = tostring(num)
 local pos, tables, done = 1, {}, false
 repeat
-    if string.sub(num,pos) ~= "." then tables[pos] = string.sub(num,pos) pos = pos+1 else done = true end
-    require("timer").sleep(250)
+    if string.sub(str,pos,pos) ~= "." then tables[pos] = string.sub(str,pos,pos) pos = pos+1 else done = true end
+    require("timer").sleep(1)
 until pos >= string.len(num) or done == true
   num = table.concat(tables,"")
-  print(num)
   return num
 end
 
@@ -27,7 +25,7 @@ command.execute = function(message,args,client)
   local m = message:reply(":ping_pong: Ping")
   if m == nil then return {success = "stfu"} end
   local latency = require("discordia").Date.fromISO(m.timestamp):toMilliseconds() - require("discordia").Date.fromISO(message.timestamp):toMilliseconds()
-  m:setContent(":ping_pong: Ping? `"..parse(latency).."ms`")
+  m:setContent(":ping_pong: Ping?")
   local edited = require("discordia").Date.fromISO(m.editedTimestamp):toMilliseconds() - require("discordia").Date.fromISO(m.timestamp):toMilliseconds()
   latency = latency + edited
   m:setContent(":ping_pong: Pong! `"..parse(latency).."ms`")
