4c336faf4eadfc0419b8710f0127c1bbfdf26166
Glitch (stellar-cosmic-lasagna) <none>
2020-03-23 11:09:01 +0000

    💃🌮 Checkpoint ./commands/ping.lua:647989/463

--- commands/ping.lua
+++ commands/ping.lua
@@ -1,5 +1,8 @@
 command = {}
 
+local discordia = require("discordia")
+local Date = discordia.Date
+
 command.info = {
   Name = "Ping",
   Alias = {},
@@ -13,6 +16,9 @@ command.execute = function(message,args,client)
   local m = message:reply(":ping_pong: Ping?")
   if m == nil then return {success = "stfu"} end
   local latency = m.createdAt - message.createdAt
+  m:setContent(":ping_pong: Ping?!")
+  print(Date:parseISO(m.editedTimestamp))
+  --latency = tonumber(m.editedTimestamp) + latency
   m:setContent(":ping_pong: Pong! `"..math.max(latency).."ms`")
   return {success = "stfu", msg = "PONG!!", emote = ":ping_pong:"}
 end
