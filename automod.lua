38fb62e66a6caf830a180b0a4933c3bf060a37b3
Glitch (stellar-cosmic-lasagna) <none>
2020-03-24 16:28:29 +0000

    ✊💈 Checkpoint ./automod.lua:647989/1365

--- automod.lua
+++ automod.lua
@@ -2,10 +2,21 @@ plugin = {}
 
 local timer = require("timer")
 
-local function addInfraction()
-
 local infractions = {}
 -- infractions[GUILDID..USERID] = {1, 2, 3}
+
+local function strike(message,data)
+  local id = message.guild.id..message.author.id
+  if infractions[id] == nil then infractions[id] = {} infractions[id][1] = os.time() return true end
+  infractions[id][1+#infractions[id]] = os.time()
+  local ten, thirty, hour = 0, 0, 0
+  if #infractions[id] >= 3 then
+    for _,items in pairs(infractions[id]) do
+      if items + 600 >= os.time() then ten = ten + 1 end
+      if items + 
+    end
+  end
+end
 
 plugin = function(message, data, client)
   local a, b = string.gsub(message.content,"\n","")
