3e5319c4880671bf091da3532d07a53113a97908
Glitch (stellar-cosmic-lasagna) <none>
2020-03-24 15:28:24 +0000

    🏯🏊🏼 Checkpoint ./commands/config.lua:647989/3043 ./commands/configcmds/general.lua:647989/1935

--- commands/config.lua
+++ commands/config.lua
@@ -26,24 +26,32 @@ command.execute = function(message,args,client)
       fields = {
         {name = "Command Prefix", value = data.general.prefix, inline = true},
         {name = "Delete Invocation Message", value = (data.general.delcmd and "Enabled." or "Disabled."), inline = true},
-        {name = "Mod Only Commands", value = (data.general.modonly and "Enabled." or "Disabled."), inline = true},
         {name = "Audit Log", value = (data.general.auditlog == "nil" and "Disabled." or (message.guild:getChannel(data.general.auditlog) == nil and "Disabled." or message.guild:getChannel(data.general.auditlog).mentionString)), inline = true},
-        {name = "Muted Role", value = (data.general.mutedrole == "nil" and "Disabled." or (message.guild:getRole(data.general.mutedrole) == nil and "Disabled." or message.guild:getRole(data.general.mutedrole).mentionString)), inline = true},
-        {name = "Moderation Log", value = (data.general.modlog == "nil" and "Disabled." or (message.guild:getChannel(data.general.modlog) == nil and "Disabled." or message.guild:getChannel(data.general.modlog).mentionString)), inline = true},
-        {name = "Moderator Roles [0]", value = "", inline = false},
         {name = "Audit Ignored Channels [0]", value = "", inline = false},
       },
       footer = {icon_url = message.author:getAvatarURL(), text = "Responding to "..message.author.tag},
       color = (message.guild:getMember(message.author.id).highestRole.color == 0 and 3066993 or message.guild:getMember(message.author.id).highestRole.color),
     }
     local num = 0
-    for _,items in pairs(data.general.modroles) do local role = message.guild:getRole(items) if role then num = num + 1 if pages[1].fields[7].value == "" then pages[1].fields[7].value = role.mentionString else pages[1].fields[7].value = pages[1].fields[7].value..", "..role.mentionString end end end
-    pages[1].fields[7].name = "Moderator Roles ["..num.."]"
-    if num == 0 then pages[1].fields[7].value = "None set!" end
-    num = 0
     for _,items in pairs(data.general.auditignore) do local role = message.guild:getChannel(items) if role then num = num + 1 if pages[1].fields[8].value == "" then pages[1].fields[8].value = role.mentionString else pages[1].fields[8].value = pages[1].fields[8].value..", "..role.mentionString end end end
-    pages[1].fields[8].name = "Audit Ignored Channels ["..num.."]"
-    if num == 0 then pages[1].fields[8].value = "None set!" end
+    pages[1].fields[4].name = "Audit Ignored Channels ["..num.."]"
+    if num == 0 then pages[1].fields[4].value = "None set!" end
+    pages[2] = {
+      title = "Moderation Settings",
+      description = "To edit a setting in this plugin, say **"..data.general.prefix..args[1].." moderation**.",
+      fields = {
+        {name = "Mod Only Commands", value = (data.general.modonly and "Enabled." or "Disabled."), inline = true},
+        {name = "Muted Role", value = "Not Configured.", inline = true},
+        {name = "Moderation Log", value = "Not Configured.", inline = true},
+        {name = "Moderator Roles", value = "Not Configured.", inline = false},
+      },
+      footer = {icon_url = message.author:getAvatarURL(), text = "Responding to "..message.author.tag},
+      color = (message.guild:getMember(message.author.id).highestRole.color == 0 and 3066993 or message.guild:getMember(message.author.id).highestRole.color),
+    }
+    if data.general.modlog ~= "nil" and message.guild:getChannel(data.general.modlog) ~= nil then pages[2].fields[3].value = message.guild:getChannel(data.general.modlog).mentionString end
+    if data.general.mutedrole ~= "nil" and message.guild:getRole(data.general.mutedrole) ~= nil then pages[2].fields[2].value = message.guild:getRole(data.general.mutedrole).mentionString end
+    local modRoles = {}
+    for _,items in pairs(data.general.modroles) do local role = message.guild:getRole(data.general.mutedrole) if role ~= nil then modRoles[1+#]
     require("/app/pages.lua").addDictionary(message,pages,message.author.id)
   end
   return {success = "stfu"}
--- commands/configcmds/general.lua
+++ commands/configcmds/general.lua
@@ -10,39 +10,12 @@ command = function(message,args,client,data)
   elseif args[3] == "delcmd" then
     data.general.delcmd = not data.general.delcmd
     return {success = true, msg = "**"..(data.general.delcmd and "Enabled" or "Disabled").."** the **delete invocation message** setting."}
-  elseif args[3] == "modonly" then
-    data.general.modonly = not data.general.modonly
-    return {success = true, msg = "**"..(data.general.modonly and "Enabled" or "Disabled").."** the **moderator only** setting."}
   elseif args[3] == "auditlog" or args[3] == "audit" then
     if args[4] == nil then return {success = false, msg = "You must provide an auditlog channel."} end
     local channel = require("/app/utils.lua").resolveChannel(message,table.concat(args," ",4))
     if channel == false then return {success = false, msg = "I couldn't find the channel you mentioned."} end
     data.general.auditlog = channel.id
     return {success = true, msg = "Set the **auditlog channel** to "..channel.mentionString.."."}
-  elseif args[3] == "modlog" then
-    if args[4] == nil then return {success = false, msg = "You must provide a modlog channel."} end
-    local channel = require("/app/utils.lua").resolveChannel(message,table.concat(args," ",4))
-    if channel == false then return {success = false, msg = "I couldn't find the channel you mentioned."} end
-    data.general.modlog = channel.id
-    return {success = true, msg = "Set the **modlog channel** to "..channel.mentionString.."."}
-  elseif args[3] == "mutedrole" or args[3] == "muted" then
-    if args[4] == nil then return {success = false, msg = "You must provide a muted role."} end
-    local channel = require("/app/utils.lua").resolveRole(message,table.concat(args," ",4))
-    if channel == false then return {success = false, msg = "I couldn't find the role you mentioned."} end
-    data.general.mutedrole = channel.id
-    return {success = true, msg = "Set the **muted role** to **"..channel.name.."**."}
-  elseif args[3] == "modrole" or args[3] == "mod" then
-    if args[4] == nil then return {success = false, msg = "You must provide a moderator role."} end
-    local channel = require("/app/utils.lua").resolveRole(message,table.concat(args," ",4))
-    if channel == false then return {success = false, msg = "I couldn't find the role you mentioned."} end
-    local found = false
-    for _,items in pairs(data.general.modroles) do if items == channel.id then found = _ break end end
-    if found ~= false then
-      table.remove(data.general.modroles,found)
-    else
-      data.general.modroles[1+#data.general.modroles] = channel.id
-    end
-    return {success = true, msg = (found == false and "Added" or "Removed").." **"..channel.name.."** as a moderator role."}
   elseif args[3] == "ignore" or args[3] == "ignored" then
     if args[4] == nil then return {success = false, msg = "You must provide a channel to ignore."} end
     local channel = require("/app/utils.lua").resolveChannel(message,table.concat(args," ",4))
