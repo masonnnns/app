e1a1ce624d757669a9c9585458e073232edcc70c
Glitch (stellar-cosmic-lasagna) <none>
2020-03-28 12:18:04 +0000

    👢🌷 Checkpoint ./commands/help.lua:647989/2035

--- commands/help.lua
+++ commands/help.lua
@@ -40,14 +40,23 @@ command.execute = function(message,args,client)
   else
     local cmdFound = require("/app/commands/"..found)
     local embed = {
-      title = data.general.prefix..cmdFound.info.Name.." Command",
+      title = cmdFound.info.Name.." Command",
       fields = {
-          
+        {name = "Description", value = cmdFound.info.Description, inline = false},
+        {name = "Usage", value = cmdFound.info.Usage, inline = false},
+        {name = "Category", value = cmdFound.info.Category, inline = true},
+        {name = "Cooldown", value = (cmdFound.info.Cooldown == nil and "2" or cmdFound.info.Cooldown).." Second"..(cmdFound.info.Cooldown == 1 and "" or "s"), inline = true},
+        {name = "Permission", value = "Member", inline = true},
       },
       footer = {icon_url = message.author:getAvatarURL(), text = "Responding to "..message.author.tag},
       color = (message.guild:getMember(message.author.id).highestRole.color == 0 and 3066993 or message.guild:getMember(message.author.id).highestRole.color),
     }
-    if cmdFound.info.PermLvl > utils.PermLvl(message,client) then embed.description = "You don't have permissions to run this command." end
+    if #cmdFound.info.Alias ~= 0 then embed.fields[1+#embed.fields] = {name = "Alias"..(#cmdFound.info.Alias == 1 and "" or "es"), value = table.concat(cmdFound.info.Alias,", "), inline = false} end
+    if cmdFound.info.PermLvl > utils.Permlvl(message,client) then embed.description = "You don't have permissions to run this command." end
+    if cmdFound.info.PermLvl == 1 then embed.fields[5].value = "Moderator" end
+    if cmdFound.info.PermLvl == 2 then embed.fields[5].value = "Administrator" end
+    if cmdFound.info.PermLvl == 3 then embed.fields[5].value = "Server Owner" end
+    if cmdFound.info.PermLvl >= 4 then embed.fields[5].value = "Bot Developer" end
     message:reply{embed = embed}
     return {success = "stfu"}
   end
