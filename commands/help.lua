feb9bdaa3b85ed2965035b3ad2ad6d0e816d3ce7
Glitch (stellar-cosmic-lasagna) <none>
2020-03-28 12:54:03 +0000

    🐽🌓 Checkpoint ./commands/help.lua:647989/2303

--- commands/help.lua
+++ commands/help.lua
@@ -38,7 +38,32 @@ command.execute = function(message,args,client)
   end
   if found ~= nil and require("/app/commands/"..found).info.PermLvl >= 4 and message.author.id ~= client.owner.id then found = nil end
   if found == nil then
-    return {success = false, msg = "command still being developed, run the command on specific commands tho"}
+    local embed = {
+      title = "Commands List",
+      description = "Say **"..data.general.prefix.."<command name>** to use a command.\nSay **"..data.general.prefix.."help <command name>** to view information about a command.",
+      fields = {},
+      footer = {icon_url = message.author:getAvatarURL(), text = "From "..message.guild.name},
+      color = (message.guild:getMember(message.author.id).highestRole.color == 0 and 3066993 or message.guild:getMember(message.author.id).highestRole.color),
+    }
+    for file, _type in fs.scandirSync("/app/commands") do
+	    if _type ~= "directory" then
+        local cmdInfo = require("/app/commands/"..file)
+        local matching
+        if cmdInfo.info.Category == nil then cmdInfo.info.Category = "Misc" end
+        for _,items in pairs(embed.fields) do if items.name == cmdInfo.info.Category then matching = _ break end end
+        if matching ~= nil then
+          embed.fields[matching].value = embed.fields[matching].value..", `"..cmdInfo.info.Name:lower().."`"
+        else
+          embed.fields[1+#embed.fields] = {name = cmdInfo.info.Category, value = "`"..cmdInfo.info.Name:lower().."`", inline = false}
+        end
+      end
+    end
+    local result = message.author:getPrivateChannel():send{embed = embed}
+    if result == nil or result == false then
+      return {success = false, msg = "I couldn't **direct message** you."}
+    else
+      return {success = true, msg = "I sent you a **direct message** with the list of commands."}
+    end
   else
     local cmdFound = require("/app/commands/"..found)
     local embed = {
