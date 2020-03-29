5f343fce55224b0fc9662873a6f34eea8f2434ba
Glitch (stellar-cosmic-lasagna) <none>
2020-03-29 14:22:08 +0000

    🎽🎱 Checkpoint ./commands/meme.lua:647989/125

--- commands/meme.lua
+++ commands/meme.lua
@@ -16,13 +16,12 @@ command.info = {
 
 command.execute = function(message,args,client)
   if config.getConfig(message.guild.id).general.funlock and message.channel.nsfw == false then return {success = false, msg = "You must be in a **NSFW Channel** to use this command. You can disable this by running **"..config.getConfig(message.guild.id).general.prefix.."config nolock**"} end
-  message.channel:broadcastTyping()
-  local result, body = http.request("GET","https://meme-api.herokuapp.com/gimme")
+  local result, body = http.request("GET","https://some-random-api.ml/meme")
   body = json.decode(body)
   if result.code ~= 200 then return {success = false, msg = "I'm having trouble fetching a meme. Try again. (HTTP "..result.code..")"} end
   message:reply{embed = {
-    title = body.title,
-    image = {url = body.url},
+    title = body.caption,
+    image = {url = body.image},
     footer = {icon_url = message.author:getAvatarURL(), text = "By Reddit • Responding to "..message.author.tag},
     color = (message.guild:getMember(message.author.id).highestRole.color == 0 and 3066993 or message.guild:getMember(message.author.id).highestRole.color),
   }}
