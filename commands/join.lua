5883cfad63fada95ddace50842c04a1cf950b4dc
Glitch (stellar-cosmic-lasagna) <none>
2020-03-29 11:12:09 +0000

    🚖🔖 Checkpoint ./commands/join.lua:647989/607

--- commands/join.lua
+++ commands/join.lua
@@ -4,22 +4,28 @@ local utils = require("/app/utils.lua")
 local config = require("/app/config.lua")
 local music = require("/app/music.lua")
 
+local discordia = require('discordia')
+local client = discordia.Client()
+discordia.extensions()
+
 command.info = {
   Name = "Join",
   Alias = {},
   Usage = "join",
   Category = "Music",
   Description = "Make AA-R0N join the voice channel you're in.",
-  PermLvl = 4,
+  PermLvl = 0,
 } 
 
 command.execute = function(message,args,client)
   local data = config.getConfig(message.guild.id)
   if data.music.enabled == false then return {success = "stfu"} end
-  if message.guild:getMember(message.author.id).voiceChannel == nil then return {success = false, msg = "You're not in a voice channel."} end
-  local connection = message.guild:getMember(message.author.id).voiceChannel:join()
-  if not connection then return {success = false, msg = "I couldn't join the **"..message.guild:getMember(message.author.id).voiceChannel.name.."** channel."} end
+  local channel = message.guild:getMember(message.author.id).voiceChannel
+  if channel == nil then return {success = false, msg = "You're not in a voice channel."} end
+  local connection = channel:join()
+  if connection == nil or connection.channel == nil then return {success = false, msg = "I couldn't join the **"..channel.name.."** channel."} end
   music.addConnection(message.guild.id,connection)
+  return {success = true, msg = "Joined **"..channel.name.."**."}
 end
 
 return command
