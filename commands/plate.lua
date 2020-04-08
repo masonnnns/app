0d2fbd90bd517ef4bb61c1781878b0b8753323f2
Glitch (aa-r0nbot) <none>
2020-04-08 13:20:52 +0000

    🎀🕋 Checkpoint ./commands/plate.lua:647989/590

--- commands/plate.lua
+++ commands/plate.lua
@@ -7,6 +7,7 @@ command.info = {
   Category = "Private",
   Description = "Translate a string into the phonetic alphabet.",
   PermLvl = 5,
+  Cooldown = 0,
 }
 
 local alphabet = {
@@ -15,7 +16,7 @@ local alphabet = {
   ["c"] = "Charlie",
   ["d"] = "Delta",
   ["e"] = "Echo",
-  ["f"] = "Foxtrot"
+  ["f"] = "Foxtrot",
   ["g"] = "Golf",
   ["h"] = "Hotel",
   ["i"] = "India",
@@ -27,18 +28,35 @@ local alphabet = {
   ["o"] = "Oscar",
   ["p"] = "Papa",
   ["q"] = "Quebec",
-  [""]
+  ["r"] = "Romeo",
+  ["s"] = "Sierra",
+  ["t"] = "Tango",
+  ["u"] = "Uniform",
+  ["v"] = "Victor",
+  ["w"] = "Whiskey",
+  ["x"] = "X-Ray",
+  ["y"] = "Yankee",
+  ["z"] = "Zulu",
 }
 
 command.execute = function(message,args,client)
   local stringSep = {}
   repeat
-    stringSep[1+#stringSep] = string.sub(table.concat(args," ",2),1+#stringSep,1+#stringSep)
+    stringSep[1+#stringSep] = string.sub(table.concat(args," ",2),1+#stringSep,1+#stringSep):lower()
     require("timer").sleep(100)
   until
   #stringSep == string.len(table.concat(args," ",2))
-  for _,items in pairs(stringSep) do print(items) end
-  return {success = "stfu", msg = ""}
+  local newString = {}
+  for _,items in pairs(stringSep) do
+    if alphabet[items] ~= nil then
+      newString[_] = alphabet[items]
+    elseif items == " " then
+      newString[_] = "**|**"
+    else
+      newString[_] = items
+    end
+  end
+  return {success = true, msg = table.concat(newString," ")}
 end
 
 return command
