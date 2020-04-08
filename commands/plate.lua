c02a6a39de945603e8202d45d8cb764a4833b3a7
Glitch (aa-r0nbot) <none>
2020-04-08 12:58:03 +0000

    🗼🍴 Checkpoint ./commands/plate.lua:647989/2034

---
+++ commands/plate.lua
@@ -0,0 +1,33 @@
+command = {}
+
+command.info = {
+  Name = "Plate",
+  Alias = {"gp"},
+  Usage = "gp [string]",
+  Category = "Private",
+  Description = "Translate a string into the phonetic alphabet.",
+  PermLvl = 5,
+}
+
+local alphabet = {
+  ["a"] = "Alpha",
+  ["b"] = "Bravo",
+  ["c"] = "Charlie",
+  ["d"] = "Delta",
+  ["e"] = "Echo",
+  ["f"] = "Foxtrot"
+  ["g"] = 
+}
+
+command.execute = function(message,args,client)
+  local stringSep = {}
+  repeat
+    stringSep[1+#stringSep] = string.sub(table.concat(args," ",2),1+#stringSep,1+#stringSep)
+    require("timer").sleep(100)
+  until
+  #stringSep == string.len(table.concat(args," ",2))
+  for _,items in pairs(stringSep) do print(items) end
+  return {success = "stfu", msg = ""}
+end
+
+return command
