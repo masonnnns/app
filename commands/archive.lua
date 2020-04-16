185621bec001ee58d0275c581de1258c610b4485
Glitch (aa-r0nbot) <none>
2020-04-16 19:27:22 +0000

    🐔☕️ Checkpoint ./commands/archive.lua:647989/1001

--- commands/archive.lua
+++ commands/archive.lua
@@ -15,13 +15,22 @@ command.info = {
 command.execute = function(message,args,client)
   if args[2] == nil then return {success = false, msg = "You must specify a number of messages."} end
   if tonumber(args[2]) == nil then return {success = false, msg = "You must specify a number of messages."} end
+  if tonumber(args[2]) <= 1 or tonumber(args[2]) >= 101 then return {success = false, msg = "The amount of messages must be greater than 1 and less than 100."} end
+  local msgs = {}
+  for a,items in pairs(message.channel:getMessages(tonumber(args[2])+1)) do if items.id == message.id then else msgs[1+#msgs] = {items.createdAt,items.author.tag.." ("..items.id.."): "..items.content} end end
+  table.sort(msgs, function(a, b)
+    return (a[1] > b[1])
+  end)
   local messages = {}
-  for a,items in pairs(message.channel:getMessages(tonumber(args[2]))) do messages[1+#messages] = items.author.tag.." ("..items.id.."): "..items.content end
+  for a,b in pairs(msgs) do messages[a] = b[2] end
   local data = require("/app/config.lua").getConfig(message.guild.id)
   data.general.archives[message.channel.id..os.time()..message.guild.id] = {date = require("/app/utils.lua").parseDateString(discordia.Date.fromSeconds(os.time()):toString(),2), messages = table.concat(messages, "\n"), channelName = message.channel.name, channelId = message.channel.id, num = #messages, guild = message.guild.name, purge = false}
   local link = "https://aa-r0nbot.glitch.me/archives/"..message.guild.id.."/"..message.channel.id..os.time()..message.guild.id
   local embed = {
-    title = ""
+    title = "Messages Archived",
+    description = "[Click Here]("..link..") to view the **"..#messages.."** archived messages.",
+    footer = {icon_url = message.author:getAvatarURL(), text = "Responding to "..message.author.tag},
+    color = (message.guild:getMember(message.author.id).highestRole.color == 0 and 3066993 or message.guild:getMember(message.author.id).highestRole.color),
   }
   message:reply{embed = embed}
   return {success = "stfu"}
