14926afe744d7fad9d7ae97eaa4386ebe2eee2a4
Glitch (aa-r0nbot) <none>
2020-04-17 10:20:49 +0000

    🐉📮 Checkpoint ./commands/archive.lua:647989/1246

--- commands/archive.lua
+++ commands/archive.lua
@@ -16,23 +16,31 @@ command.execute = function(message,args,client)
   if args[2] == nil then return {success = false, msg = "You must specify a number of messages."} end
   if tonumber(args[2]) == nil then return {success = false, msg = "You must specify a number of messages."} end
   if tonumber(args[2]) <= 1 or tonumber(args[2]) >= 101 then return {success = false, msg = "The amount of messages must be greater than 1 and less than 100."} end
+  local embed = {
+    title = "Archiving Messages",
+    description = "<a:AARONLOADING:552079960022581248> Please wait, the archive will be available shortly.",
+    footer = {icon_url = message.author:getAvatarURL(), text = "Responding to "..message.author.tag},
+    color = (message.guild:getMember(message.author.id).highestRole.color == 0 and 3066993 or message.guild:getMember(message.author.id).highestRole.color),
+  }
+  local getMessages = message.channel:getMessages(tonumber(args[2])+1)
+  local botReply = message:reply{embed = embed}
   local msgs = {}
-  for a,items in pairs(message.channel:getMessages(tonumber(args[2])+1)) do if items.id == message.id then else msgs[1+#msgs] = {items.createdAt,items.author.tag.." ("..items.id.."): "..items.content} end end
+  for a,items in pairs(getMessages) do if items.id == message.id then else msgs[1+#msgs] = {items.createdAt,items.author.tag.." ("..items.author.id.."): "..items.content} end end
   table.sort(msgs, function(a, b)
-    return (a[1] > b[1])
+    return (a[1] < b[1])
   end)
   local messages = {}
   for a,b in pairs(msgs) do messages[a] = b[2] end
   local data = require("/app/config.lua").getConfig(message.guild.id)
   data.general.archives[message.channel.id..os.time()..message.guild.id] = {date = require("/app/utils.lua").parseDateString(discordia.Date.fromSeconds(os.time()):toString(),2), messages = table.concat(messages, "\n"), channelName = message.channel.name, channelId = message.channel.id, num = #messages, guild = message.guild.name, purge = false}
   local link = "https://aa-r0nbot.glitch.me/archives/"..message.guild.id.."/"..message.channel.id..os.time()..message.guild.id
-  local embed = {
+  embed = {
     title = "Messages Archived",
     description = "[Click Here]("..link..") to view the **"..#messages.."** archived messages.",
     footer = {icon_url = message.author:getAvatarURL(), text = "Responding to "..message.author.tag},
-    color = (message.guild:getMember(message.author.id).highestRole.color == 0 and 3066993 or message.guild:getMember(message.author.id).highestRole.color),
+    color = botReply.embed.color,
   }
-  message:reply{embed = embed}
+  botReply:setEmbed(embed)
   return {success = "stfu"}
 end
 
