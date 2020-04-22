f7927c03adb1bc4215956906f73f153fd48fc33f
Glitch (aa-r0nbot) <none>
2020-04-22 20:00:39 +0000

    🌅🍉 Checkpoint ./commands/getroles.lua:647989/1923

--- commands/getroles.lua
+++ commands/getroles.lua
@@ -15,27 +15,27 @@ command.info = {
 }
 
 local bindings = {
-  255 = "467891231087919115",
-  170 = "469392300536102922",
-  160 = "468847974462783509",
-  150 = "468831154787581955",
-  140 = "468831165122347038",
-  130 = "468831168804945921",
-  120 = "468831168804945921",
-  110 = "468108245714731028",
-  100 = "468833667578331157",
-  90 = "468108495833530408",
-  80 = "467888166574227486",
-  70 = "467887036250980352",
-  69 = "678494383682879492",
-  60 = "467886605118472202",
-  50 = "467886500294426644",
-  45 = "685329271786438706",
-  40 = "467885360630988810",
-  30 = "467887348978417703",
-  20 = "469034307294330891",
-  10 = "467886277749112832",
-  0 = "467886277749112832",
+  [255] = "467891231087919115",
+  [170] = "469392300536102922",
+  [160] = "468847974462783509",
+  [150] = "468831154787581955",
+  [140] = "468831165122347038",
+  [130] = "468831168804945921",
+  [120] = "468831168804945921",
+  [110] = "468108245714731028",
+  [100] = "468833667578331157",
+  [90] = "468108495833530408",
+  [80] = "467888166574227486",
+  [70] = "467887036250980352",
+  [69] = "678494383682879492",
+  [60] = "467886605118472202",
+  [50] = "467886500294426644",
+  [45] = "685329271786438706",
+  [40] = "467885360630988810",
+  [30] = "467887348978417703",
+  [20] = "469034307294330891",
+  [10] = "467886277749112832",
+  [0] = "467886277749112832",
 }
 
 command.execute = function(message,args,client)
@@ -61,8 +61,27 @@ command.execute = function(message,args,client)
       break
     end
   end
-  if message.member.roles:get(bindings[])
-  return {success = true, msg = groupInfo.Role}
+  local added, removed = {}, {}
+  if message.member.roles:get(bindings[groupInfo.Rank]) == nil then added[1+#added] = groupInfo.Role message.member:addRole(bindings[groupInfo.Rank]) end
+  for a,b in pairs(bindings) do
+    if message.member.roles:get(b) ~= nil and a ~= groupInfo.Rank then
+      removed[1+#removed] = message.member.roles:get(b).name
+      message.member:removeRole(b)
+      require("timer").sleep(500)
+    end
+  end
+  if #added + #removed == 0 then return {success = false, msg = "No changes were made."} end
+  local embed = {
+    title = "Roles Changed",
+    fields = {
+      {name = "Added ["..#added.."]", value = (#added == 0 and "None!" or table.concat(added,", ")), inline = false},
+      {name = "Removed ["..#removed.."]", value = (#removed == 0 and "None!" or table.concat(removed,", ")), inline = false},
+    },
+    footer = {icon_url = message.author:getAvatarURL(), text = "Responding to "..message.author.tag},
+    color = (message.member:getColor() == 0 and 3066993 or message.member:getColor().value),
+  }
+  message:reply{embed = embed}
+  return {success = "stfu"}
 end
 
 return command
