acc4bd7ef957c58f8a5e8afec4430ad294f096cb
Glitch (aa-r0nbot) <none>
2020-04-23 19:30:48 +0000

    📈🐁 Checkpoint ./commands/announce.lua:647989/1697

--- commands/announce.lua
+++ commands/announce.lua
@@ -18,23 +18,29 @@ command.info = {
 command.execute = function(message,args,client)
   if message.guild.id ~= "467880413981966347" then return {success = "stfu"} end
   local channel = utils.resolveChannel(message,(args[2] == nil and message.channel.id or args[2]))
-  if channel == false then channel = message.channel end
-  local webhook = channel:createWebhook("TeaCore")
+  if channel == false then return {success = false, msg = "I couldn't find the channel you mentioned."} end
+  local webhook = channel:createWebhook("TeaCore",message.guild.iconURL)
   local headers = {
     {"Content-Type", "application/json"} 
   }
+  local text = string.sub(message.content,string.len(args[1])+string.len(args[2])+4)
   local embed = {
     embeds = {
       {
-        title = "title",
-        description = "DOG",
-        color = 0xff7662,
+        description = text,
+        color = 0xfdb14d,
         timestamp = require("discordia").Date():toISO('T', 'Z'),
       }
     }
   }
   webhook:setAvatar(message.guild.iconURL)
+  if args[#args]:lower() == "here" or args[#args]:lower() == "everyone" then
+    text = string.sub(text,1,string.len(text) - string.len(args[#args]))
+    embed.embeds[1].description = text
+    embed.content = "@"..args[#args]:lower()
+  end
   local response, body = require("coro-http").request("POST","https://canary.discordapp.com/api/webhooks/"..webhook.id.."/"..webhook.token,headers,require("json").encode(embed))
-  return {success = true, msg = webhook.token.." - "..webhook.channelId.." - "..webhook.guildId}
+  webhook:delete()
+  return {success = true, msg = "Sent the message to **"..channel.name.."**!"}
 end
 return command
