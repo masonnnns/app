70738ff58d2be9fede68cf9e699153621b02d8ea
Glitch (aa-r0nbot) <none>
2020-04-26 21:20:43 +0000

    🎒💺 Checkpoint ./server.lua:647989/33407 ./commands/verify.lua:647989/1153

--- commands/verify.lua
+++ commands/verify.lua
@@ -1,37 +1,23 @@
 command = {}
 
-local utils = require("/app/utils.lua")
-local config = require("/app/config.lua")
-local http = require('coro-http')
-local json = require("json")
-
 command.info = {
-  Name = "Verify",
-  Alias = {},
-  Usage = "verify",
-  Category = "Private",
-  Description = "Verify your Roblox account with Discord.",
-  PermLvl = 0,
+  Name = "Addmod",
+  Alias = {"mod"},
+  Usage = "addmod <member>",
+  Category = "Administration",
+  Description = "Give a member moderator permissions.",
+  PermLvl = 2,
 } 
 
 command.execute = function(message,args,client)
-  if message.guild.id ~= "467880413981966347" then return {success = "stfu"} end
-  local result, body = http.request("GET","https://verify.eryn.io/api/user/"..message.author.id)
-  body = json.decode(body)
-  if body.status == "ok" then
-    local name = body.robloxUsername
-    local newNameR, newNameBody = http.request("GET","https://api.roblox.com/users/"..body.robloxId)
-    if newNameR.code == 200 then name = json.decode(newNameBody).Username end
-    message.member:setNickname(name)
-    if message.member.roles:get("515647391676891138") == nil then message.member:addRole("515647391676891138") end
-    return {success = true, msg = "You've been verified as **"..name.."**."}
-  else
-    if body.errorCode ~= nil and body.errorCode == 404 then
-      return {success = false, msg = "You're not verified with RoVer! Verify here: <https://verify.eryn.io/>"}
-    else
-      return {success = false, msg = "Verification Failed!```ERR: "..body.error:upper().."```"}
-    end
-  end
+  if args[2] == nil then return {success = false, msg = "You must specify a member."} end
+  local user = utils.resolveUser(message,table.concat(args," ",2))
+  if user == false then return {success = false, msg = "I couldn't find the user you mentioned."} end
+  if user.id == client.user.id then return {success = false, msg = "I cannot give myself moderator permissions."} end
+  if utils.Permlvl(message,client,user.id) >= 1 then return {success = false, msg = "**"..user.tag.."** already has moderator permissions."} end
+  local data = config.getConfig(message.guild.id)
+  data.general.mods[1+#data.general.mods] = user.id
+  return {success = true, msg = "**"..user.tag.."** has been given moderator permissions."}
 end
 
 return command
