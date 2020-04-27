a47bf2a49d9f2320c9f549346e17cd825c34b8d0
Glitch (aa-r0nbot) <none>
2020-04-27 21:18:43 +0000

    🚪🏜 Checkpoint ./commands/whois.lua:647989/1185 ./utils.lua:647989/296

--- commands/whois.lua
+++ commands/whois.lua
@@ -16,15 +16,23 @@ command.execute = function(message,args,client)
   local res, body = require("coro-http").request("GET","https://verify.eryn.io/api/user/"..member.id)
   if res.code == 404 then return {success = false, msg = (member.id == message.author.id and "You're not verified with RoVer. Verify here: <https://verify.eryn.io" or "**"..member.tag.."** isn't verified with RoVer.")} end
   if res.code ~= 200 then return {success = false, msg = "There was a problem checking the verification registry. Try again."} end
+  local userId = require("json").decode(body).robloxId
   local embed = {
     title = require("json").decode(body).robloxUsername,
     url = "https://www.roblox.com/users/"..require("json").decode(body).robloxId.."/profile",
   }
-  res, body = require("coro-http").request("GET","https://users.roblox.com/v1/users/"..require("json").decode(body).robloxId)
+  res, body = require("coro-http").request("GET","https://users.roblox.com/v1/users/"..userId)
   if res.code ~= 200 then return {success = false, msg = "There was a problem with the Roblox API. Try again."} end
   body = require('json').decode(body)
-  embed.description = string.sub(body.description,1,1500)..(string.len(body.description) > 1500 and "..." or "")
+  embed.fields = {}
+  embed.fields[1] = {name = "Description", value = string.sub(body.description,1,1500)..(string.len(body.description) > 1500 and "..." or ""), inline = false}
+  if body.isBanned then embed.description = "🔨 **This user is banned from Roblox.**" end
   embed.thumbnail = {url = "https://assetgame.roblox.com/Thumbs/Avatar.ashx?username="..embed.title}
+  embed.footer = {icon_url = message.author:getAvatarURL(), text = "Responding to "..message.author.tag},
+  embed.timestamp = require("discordia").Date():toISO('T', 'Z'),
+  if conifg.group 
+  res, body = require("coro-http").request("GET","https://api.roblox.com/users/"..userId.."/groups")
+  if res.code ~= 200 then return {success = false, msg = "There was a problem with the Roblox API. Try again."} end
   message:reply{embed = embed}
   return {success = "stfu"}
 end
